b4c664b64c6b7611f1be042263cae84d15da8c10
Phichayaphak Phengphan <phichayaphakspy@hotmail.co.th>
2023-09-27 22:59:16 +0700

    Setup body parser

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(express.json());
+
 app.get("/health-check", (req, res) => {
   res.send(`${Date()}: OK`);
 });
 
-app.use("/api/v1", router)
+app.use("/api/v1", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT: ${PORT}`);
