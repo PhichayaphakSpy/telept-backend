8dce2c9ca1b0b2fe5e71e62ed2a77c95f073d732
Phichayaphak Phengphan <phichayaphakspy@hotmail.co.th>
2023-09-29 05:40:34 +0700

    allow cors to all origin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import router from "./routers";
+import cors from "cors";
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+// const corsOption = {
+//   origin: "http://localhost:*",
+//   credentials: true,
+// };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/health-check", (req, res) => {
